f9800333f1a5af16a5976ea21e38fcb3c4cb1496
Yujin Hong <yujhong@microsoft.com>
2018-08-20 16:59:50 +0000

    Added ServiceFactory
    Added ServiceFactory

--- InventoryApp/Models/TCconfig.cs
+++ InventoryApp/Models/TCconfig.cs
@@ -8,12 +8,20 @@ namespace TrafficCaseApp.Models
     public class TCConfig
     {
         public CosmosConfig CosmosConfig { get; set; }
+        public StorageConfig StorageConfig { get; set; }
     }
         
     public class CosmosConfig
     {
         public string Uri { get; set; }
         public string Key { get; set; }
+    }
+
+    public class StorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string EndPointSuffix { get; set; }
     }
 
 }
---
+++ InventoryApp/Services/ServiceFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Documents.Client;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrafficCaseApp.Models;
+
+namespace TrafficCaseApp.Services
+{
+    public class ServiceFactory
+    {
+        private TCConfig config;
+        public ServiceFactory(TCConfig config)
+        {
+            this.config = config;
+        }
+        public CloudStorageAccount CreateCloudStorageAccount() => 
+            new CloudStorageAccount(
+                new StorageCredentials(config., Configuration["Storage:AccountKey"]);
+        Func<IServiceProvider, CloudStorageAccount> storageAcctFunc =
+             p => new CloudStorageAccount(credentials, "core.usgovcloudapi.net", true);
+        public DocumentClient CreateDocumentClient() => new DocumentClient(new Uri(this.config.CosmosConfig.Uri), this.config.CosmosConfig.Key);
+
+    }
+}
